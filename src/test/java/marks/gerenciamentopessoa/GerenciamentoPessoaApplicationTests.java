4caa34e5ad7f67d07f5fe8f3038335555fab6414
Andre <andrem4rks@gmail.com>
2022-10-06 14:15:51 -0300

    first commit

---
+++ src/test/java/marks/gerenciamentopessoa/GerenciamentoPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package marks.gerenciamentopessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoPessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
