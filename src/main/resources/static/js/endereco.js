d078cbaa9360db64dd287ee58e7574dab2f8b708
Marks <andrem4rks@gmail.com>
2022-10-27 23:45:32 -0500

    .

--- src/main/resources/static/js/endereco.js
+++ src/main/resources/static/js/endereco.js
@@ -4,25 +4,22 @@ const showData = (result) => {
     for(const campo in result) {
         if(document.querySelector('#'+campo)) {
             document.querySelector("#"+campo).value = result[campo]
+            campo.addClass("is-valid")
         }
-        
-
     }
 }
 
 cep.addEventListener("blur",()=>{
     let search = cep.value.replace("-" , "")
-    const options = {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'default'
-    }
-
-    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-    .then(response=>{response.json()
-        .then( data => showData(data) )
+    $.ajax({
+        url: 'https://api.postmon.com.br/v1/cep/' + search,
+        dataType: "json",
+        crossDomain: true,
+            statusCode: {
+                200: function(data) {
+                    console.log(data);
+                }
+            }
     })
-    .catch(e => console.log('Deu erro: '+ e.message))
 
-    console.log(search)
 })
